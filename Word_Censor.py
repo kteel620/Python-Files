7497832eb61e59fbf724150a45c7f3d4a9b17873
Keshav T <k.teeluck1@gmail.com>
2017-08-15 20:35:47 -0400

    Add files via upload

--- Word_Censor.py
+++ Word_Censor.py
@@ -1,17 +1,17 @@
-def some_function(sent):
-    curse_words = ["lame"]
+def some_function(sentence):
+    bad_words = ["lame"]
 
     lister = []
-    for i in sent:
+    for i in sentence:
         lister.append(i)
 
     new_sent = "".join(lister)
 
-    for a in curse_words:
+    for a in bad_words:
         if a in new_sent:
             new_sent = new_sent.replace(a, "*" * len(a))
 
     print(new_sent)
 
 
-some_function("It's so lame that today is sunday night. I mean... What happened to our weekend?")
+some_function("It's so lame that today is already sunday night. I mean... What happened to our weekend?")
