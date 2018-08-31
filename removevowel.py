ad2edc195ec5b6b6246d5cdd1eea13b25b1b410a
Keshav T <k.teeluck1@gmail.com>
2018-08-31 15:04:27 -0400

    Add files via upload

---
+++ removevowel.py
@@ -0,0 +1,17 @@
+
+def rmv_vowel():
+    letter = ["a", "e", "i", "o", "u","A","E","I","O","U"]
+    new_letters = []
+    sentense = input("Type out a sentence to be returned the same sentence without vowels. ")
+    new_sent = []
+    for g in sentense:
+        if g in letter:
+            new_sent.append("-")
+        else:
+            new_sent.append(g)
+    print("".join(new_sent))
+
+
+
+rmv_vowel()
+
